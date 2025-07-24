7cd76b19e3e8a6d23a533e3a8fec99dc9d05364e
mauricedeschamps <mementomorikatsu@gmail.com>
2025-07-24 18:50:56 +0900

    sw.js の更新

--- sw.js
+++ sw.js
@@ -3,15 +3,8 @@ const urlsToCache = [
   '/',
   'index.html',
   'manifest.json',
-  // アイコンファイルを追加
-  'icons/icon-72x72.png',
-  'icons/icon-96x96.png',
-  'icons/icon-128x128.png',
-  'icons/icon-144x144.png',
-  'icons/icon-152x152.png',
-  'icons/icon-192x192.png',
-  'icons/icon-384x384.png',
-  'icons/icon-512x512.png'
+  'icon-192x192.png',
+  'icon-512x512.png'
 ];
 
 // インストールイベント
